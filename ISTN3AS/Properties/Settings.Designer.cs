4e208658b4e356d4ab2cdf066b720c072c5b04af
nashlin17 <nashlinmoodly@gmail.com>
2020-08-10 23:58:52 +0200

    Revert "Merge branch 'master' of https://github.com/nashlin17/ISTN-Project"
    This reverts commit 6c63df1e98266897b256b3385d8e33b6d42b0eb2, reversing
    changes made to 7c3b1b7d4a7bc10192f83276eafdc5bb0b7740b5.

--- ISTN3AS/Properties/Settings.Designer.cs
+++ ISTN3AS/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ISTN3AS.Properties {
-    
-    
+namespace ISTN3AS.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=146.230.177.46\\IST3;Initial Catalog=group6;User ID=group6;Password=h2" +
-            "d9j")]
-        public string PosConnectionString {
-            get {
-                return ((string)(this["PosConnectionString"]));
             }
         }
     }
